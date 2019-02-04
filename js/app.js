8b144678485d24590a0e73c9a17f816569bc218a
Francesco Casadei <fran.casadei@gmail.com>
2019-02-04 14:24:04 +0100

    obstacles + one player

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ $(".dropbox").droppable();
 
 $( ".dropbox" ).draggable({ snap: ".dropbox", grid: [40,40] });
 
-*/
+
 
 
 
@@ -40,12 +40,74 @@ $( ".dropbox" ).draggable({ snap: ".dropbox", grid: [40,40] });
             + " class=\"dropbox \"></div>"
         );
 }
-$(".dropbox").droppable();
+$(".dropbox").droppable();*/
 
-$( "#player-one" ).draggable({ snap: ".dropbox", grid: [40,40] });
+var grid = document.getElementById("grid-box");
+
+for (var i = 1; i <= 100; i++) {
+  var square = document.createElement("div");
+  square.className = 'square';
+  square.id = 'square' + i;
+  grid.appendChild(square);
+}
+
+var obstacles = []; // Stores the squares index placed
+
+while (obstacles.length < 10) { // Stop only if 20 squares are added
+  var randomIndex = parseInt(99 * Math.random()); // Generate a random number between 0 and 99
+
+  // Only add the square if it doesn't exist already
+  if (obstacles.indexOf(randomIndex) === -1) {
+    obstacles.push(randomIndex);
+    document.getElementById('square' + randomIndex).style.backgroundColor = 'yellow';
+
+  }
+}
+
+var playerOne = []; // Stores the squares index placed
+
+while (playerOne.length < 1) { // Stop only if 2 squares are added
+  var randomIndex = parseInt(99 * Math.random()); // Generate a random number between 0 and 99
+
+  // Only add the square if it doesn't exist already
+  if (playerOne.indexOf(randomIndex) === -1) {
+    playerOne.push(randomIndex);
+    document.getElementById('square' + randomIndex).style.backgroundColor = 'red';
+
+  }
+}
+
+//$("#square9").draggable({axis: "x,y"});
 
 
 
+
+
+
+
+
+
+
+
+
+/*
+var playerTwo = []; // Stores the squares index placed
+
+while (playerTwo.length < 1) { // Stop only if 2 squares are added
+  var randomIndex = parseInt(99 * Math.random()); // Generate a random number between 0 and 99
+
+  // Only add the square if it doesn't exist already
+  if (playerTwo.indexOf(randomIndex) === -1) {
+    playerTwo.push(randomIndex);
+    document.getElementById('square' + randomIndex).style.borderColor = 'blue';
+  }
+}
+*/
+
+//$( "#player-one" ).draggable({ snap: ".dropbox", grid: [40,40] });
+
+
+//$("#player-one").draggable({ snap: ".dropbox", grid: [40,40] });
 
 
 /* DRAW THE GRID 
